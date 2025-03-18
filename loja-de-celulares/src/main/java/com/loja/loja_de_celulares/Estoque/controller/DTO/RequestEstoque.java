ce2bb5dbdca71298b1c51c68f649dfdd128b5fc4
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-17 22:36:23 -0300

    ADD: Pacote Controller e DTO criados + inserção de classe Request

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/DTO/RequestEstoque.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/DTO/RequestEstoque.java
@@ -1,4 +1,18 @@
 package com.loja.loja_de_celulares.Estoque.controller.DTO;
 
-public record RequestEstoque() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestEstoque(
+
+        @NotBlank(message = "O código do produto não pode estar vazio.")
+        String codigo_produto,
+
+        @NotBlank(message = "O nome do produto não pode estar vazio.")
+        String nome_produto,
+
+        @NotNull(message = "O preço em centavos é obrigatório.")
+        Integer preco_em_centavos,
+
+        Boolean ativo){
 }
