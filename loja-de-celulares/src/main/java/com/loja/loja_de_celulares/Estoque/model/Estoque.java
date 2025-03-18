01c545a3f5d993ce1dbcb16b2929a9949abfed25
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-17 22:37:44 -0300

    ADD: Pacote model + classe principal estoque criados

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/model/Estoque.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/model/Estoque.java
@@ -1,4 +1,29 @@
 package com.loja.loja_de_celulares.Estoque.model;
 
+import com.loja.loja_de_celulares.Estoque.controller.DTO.RequestEstoque;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="estoque")
+@Entity(name="estoque")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "codigo_produto")
 public class Estoque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String codigo_produto;
+    private String nome_produto;
+    private Integer preco_em_centavos;
+    private Boolean ativo;
+
+    public Estoque(RequestEstoque requestEstoque) {
+        this.nome_produto = requestEstoque.nome_produto();
+        this.preco_em_centavos = requestEstoque.preco_em_centavos();
+        this.ativo = true;
+    }
 }
+
