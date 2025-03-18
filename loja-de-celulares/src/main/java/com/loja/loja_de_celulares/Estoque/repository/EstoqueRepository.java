0ccf8b46398773f628a77df382dd52ca71f74e33
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-17 22:38:44 -0300

    ADD: PACOTE + CLASSE REPOSITORY CRIADOS

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/repository/EstoqueRepository.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/repository/EstoqueRepository.java
@@ -1,4 +1,9 @@
 package com.loja.loja_de_celulares.Estoque.repository;
 
-public interface EstoqueRepository {
+import com.loja.loja_de_celulares.Estoque.model.Estoque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EstoqueRepository extends JpaRepository<Estoque, String> {
 }
