b0ed9ed4b9c7588188e15c0abf44c09187130cd4
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-19 22:21:53 -0300

    ADD: Classe controller e função de findByActiveTRue implementados

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/EstoqueController.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/EstoqueController.java
@@ -1,4 +1,21 @@
 package com.loja.loja_de_celulares.Estoque.controller;
 
+
+import com.loja.loja_de_celulares.Estoque.repository.EstoqueRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estoque")
 public class EstoqueController {
+
+
+    private EstoqueRepository estoqueRepository;
+
+    public ResponseEntity consultaEStoque(){
+        var consultaEstoque = estoqueRepository.findAllByAtivoTrue();
+        return ResponseEntity.ok(consultaEstoque);
+    }
+
 }
