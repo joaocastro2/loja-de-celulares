c85d27bfc2d88b849f0eed8a099160c02b44220e
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-20 20:15:20 -0300

    ADD: Método save no estoque e correção de bug

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/EstoqueController.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/controller/EstoqueController.java
@@ -1,21 +1,33 @@
 package com.loja.loja_de_celulares.Estoque.controller;
 
 
+import com.loja.loja_de_celulares.Estoque.DTO.RequestEstoqueDTO;
+import com.loja.loja_de_celulares.Estoque.model.Estoque;
 import com.loja.loja_de_celulares.Estoque.repository.EstoqueRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/estoque")
 public class EstoqueController {
 
-
+    @Autowired
     private EstoqueRepository estoqueRepository;
 
+    @PostMapping
+    public ResponseEntity cadastroEstoque(@RequestBody @Valid RequestEstoqueDTO novoProduto){
+        Estoque estoque = new Estoque(novoProduto);
+        estoqueRepository.save(estoque);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
     public ResponseEntity consultaEStoque(){
         var consultaEstoque = estoqueRepository.findAllByAtivoTrue();
         return ResponseEntity.ok(consultaEstoque);
     }
+
 
 }
