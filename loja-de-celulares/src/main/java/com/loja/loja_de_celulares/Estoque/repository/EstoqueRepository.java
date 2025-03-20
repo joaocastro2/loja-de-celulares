97ca8bdf79817c449707bd03360c36a4df0334d0
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-19 22:23:17 -0300

    ---

--- loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/repository/EstoqueRepository.java
+++ loja-de-celulares/src/main/java/com/loja/loja_de_celulares/Estoque/repository/EstoqueRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/*Herda uma série de métodos prontos que facilitam a interação com o banco de dados, como operações de CRUD (Create, Read, Update, Delete).
+A regra definida neste negócio é Usá-la para funções personalizadas, como buscas, por exemplo, e implementá-las na controller */
+
 public interface EstoqueRepository extends JpaRepository<Estoque, String> {
+    List<Estoque> findAllByAtivoTrue();
 }
