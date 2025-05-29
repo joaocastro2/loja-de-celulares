3507ce18f05b32ef55a9c23a260920c3ded6846e
joaocastro2 <joaocastrocntt06@gmail.com>
2025-05-29 19:59:20 -0300

    migrations created and working

---
+++ loja-de-celulares/src/main/resources/db/migration/V1__create_table_stock.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stock (
+    product_id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    product_name TEXT NOT NULL,
+    price_in_cents INT NOT NULL,
+    amount INT DEFAULT 0,
+    active BOOLEAN
+);
