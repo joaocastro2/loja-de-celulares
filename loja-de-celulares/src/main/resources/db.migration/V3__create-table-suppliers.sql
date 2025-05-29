6a224a18c0ae1304946fa1dd0246a0fbba532932
joaocastro2 <joaocastrocntt06@gmail.com>
2025-05-29 19:58:51 -0300

    ---

--- loja-de-celulares/src/main/resources/db.migration/V3__create-table-suppliers.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE suppliers (
-    supplier_id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    supplier_name TEXT NOT NULL,
-    description TEXT,
-    category VARCHAR(15),
-    active BOOLEAN
-);
