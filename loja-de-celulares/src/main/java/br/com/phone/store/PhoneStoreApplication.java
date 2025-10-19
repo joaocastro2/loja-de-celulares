499dde130526946c8d97d6296d7df5830facd2b8
joaocastro2 <joaocastrocntt06@gmail.com>
2025-10-18 23:14:32 -0300

    ---

--- loja-de-celulares/src/main/java/br/com/phone/store/PhoneStoreApplication.java
+++ loja-de-celulares/src/main/java/br/com/phone/store/PhoneStoreApplication.java
@@ -1,6 +1,5 @@
 package br.com.phone.store;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,11 +9,5 @@ public class PhoneStoreApplication {
 	public static void main(String[] args) {
 		// Start the Spring context
 		var context = SpringApplication.run(PhoneStoreApplication.class, args);
-
-		// Pass the content to JavaFX
-		JavaFxApplication.setSpringContext(context);
-
-		// Start the JavaFX apllication
-		Application.launch(JavaFxApplication.class, args);
 	}
 }
