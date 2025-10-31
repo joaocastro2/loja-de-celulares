ea8d748438206d5690f40f719937cc985f2603fb
joaocastro2 <joaocastrocntt06@gmail.com>
2025-10-31 17:36:11 -0300

    ---

--- loja-de-celulares/src/main/java/br/com/phone/store/infra/controller/AuthController.java
+++ loja-de-celulares/src/main/java/br/com/phone/store/infra/controller/AuthController.java
@@ -9,16 +9,14 @@ import br.com.phone.store.tables.users.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final UsersRepository repository;
