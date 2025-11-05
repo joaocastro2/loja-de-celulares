914d29bfd8c971b79cb0b4af8c1b177eb33591c3
joaocastro2 <joaocastrocntt06@gmail.com>
2025-11-04 21:11:17 -0300

    Cors Validation added

--- loja-de-celulares/src/main/java/br/com/phone/store/tables/suppliers/controller/SuppliersController.java
+++ loja-de-celulares/src/main/java/br/com/phone/store/tables/suppliers/controller/SuppliersController.java
@@ -6,11 +6,9 @@ import br.com.phone.store.tables.suppliers.repository.SuppliersRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/suppliers")
 public class SuppliersController {
